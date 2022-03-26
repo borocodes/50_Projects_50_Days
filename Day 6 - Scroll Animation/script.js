b4ae75450295c06a39ac6d1ec9b26a12aa6d4cbd
Jonathan Borofsky <jonborofsky@gmail.com>
2022-03-26 12:09:37 -0500

    Day 7 CSS Completed

--- Day 6 - Scroll Animation/script.js
+++ Day 6 - Scroll Animation/script.js
@@ -2,6 +2,7 @@ const boxes = document.querySelectorAll('.box')
 
 window.addEventListener('scroll', checkBoxes)
 
+//calls viewport-determined number of boxes as soon as the page loads. Without it none of the boxes roll in until a scroll event is detected 
 checkBoxes()
 
 function checkBoxes() {
